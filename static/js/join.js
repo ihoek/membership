4f95f28eb9d00c417087047b76f09fe2e8c13997
ihoek <ihoek679@naver.com>
2025-02-12 09:24:19 +0900

    회원가입 수정

--- static/js/join.js
+++ static/js/join.js
@@ -1,34 +1,29 @@
-//변수 선언 - DOM
+// 변수 선언 - DOM
 const search_id = document.querySelector(".search_id");
 const search_pw = document.querySelector(".search_pw");
 const search_membership = document.querySelector(".search_membership");
-const loginButton = document.querySelector(".login");
-const input_id = document.getElementById("id");
-const input_pw = document.getElementById("pw");
+const id = document.getElementById("id");
+const pw = document.getElementById("pw");
+const login = document.getElementById("login");
 
-//local
-// let local_data_map = JSON.parse(localStorage.getItem("data"));
-// let local_id = [];
-// let local_pw = [];
+let local_data_map = JSON.parse(localStorage.getItem("data")) || [];
 
-// local_data_map.map((element) => {
-//   local_id.push(element.id);
-//   local_pw.push(element.pw);
-// });
+let isIdValid = false;
+let isPwValid = false;
 
-//sub menu 이동 - 아이디
+// sub menu 이동 - 아이디
 search_id.addEventListener("click", () => {
   console.log("아이디 찾기 클릭");
   window.location.href = "/searchingid";
 });
 
-//sub menu 이동 - 비밀번호
+// sub menu 이동 - 비밀번호
 search_pw.addEventListener("click", () => {
   console.log("비밀번호 찾기 클릭");
   window.location.href = "/searchingpw";
 });
 
-//sub menu 이동 - 회원가입
+// sub menu 이동 - 회원가입
 search_membership.addEventListener("click", () => {
   console.log("회원가입 클릭");
   window.location.href = "/membership";
@@ -37,11 +32,10 @@ search_membership.addEventListener("click", () => {
 fetch("/userinfo")
   .then((response) => response.json())
   .then((data) => {
-    const updata_data = JSON.parse(JSON.stringify(data)); //data 형태변환
+    const updata_data = JSON.parse(JSON.stringify(data)); // data 형태변환
     let local_data = JSON.parse(localStorage.getItem("data")) || [];
-    //console.log("local_data", local_data);
 
-    //해당 하는 값이 이미 로컬에 있는 경우 안됨
+    // 해당 하는 값이 이미 로컬에 있는 경우 안됨
     let exists = local_data.some((item) => item.id === updata_data.id);
 
     if (!exists) {
@@ -49,7 +43,57 @@ fetch("/userinfo")
       localStorage.setItem("data", JSON.stringify(local_data)); // 로컬 스토리지 저장
     }
   })
-
   .catch((e) => {
     console.error("error", e);
   });
+
+// 아이디 확인 함수
+function printid() {
+  let userid = id.value;
+  // 아이디를 로컬 데이터에서 찾기
+  let found = false;
+  local_data_map.forEach((element) => {
+    if (userid === element.id) {
+      console.log("아이디 찾음", element);
+      isIdValid = true;
+      found = true;
+    }
+  });
+
+  if (!found) {
+    isIdValid = false;
+  }
+  checkForm();
+}
+
+// 비밀번호 확인 함수
+function printpw() {
+  let userpw = pw.value;
+  // 비밀번호를 로컬 데이터에서 찾기
+  let found = false;
+  local_data_map.forEach((element) => {
+    if (userpw === element.pw) {
+      console.log("비밀번호 찾음", element);
+      isPwValid = true;
+      found = true;
+    }
+  });
+
+  if (!found) {
+    isPwValid = false;
+  }
+  checkForm();
+}
+
+// 폼 유효성 검사
+function checkForm() {
+  if (isIdValid && isPwValid) {
+    login.disabled = false; // 아이디와 비밀번호가 모두 유효하면 버튼 활성화
+  } else {
+    login.disabled = true; // 하나라도 유효하지 않으면 버튼 비활성화
+  }
+}
+
+window.onload = function () {
+  login.disabled = true; // 페이지 로드 시 버튼 비활성화
+};
--- static/js/membership.js
+++ static/js/membership.js
@@ -2,10 +2,20 @@
 const input_id = document.getElementById("input_id");
 const input_pw = document.getElementById("input_pw");
 const input_repw = document.getElementById("input_repw");
+const btn_join = document.getElementById("btn_join");
 
 let id = "";
 let pw = "";
 let repw = "";
+
+let local_data = JSON.parse(localStorage.getItem("data"));
+let local_data_id = [];
+local_data.map((element) => {
+  local_data_id.push(element.id);
+});
+
+let isIdValid = false; // 아이디 중복 체크 여부
+let isPwValid = false; // 비밀번호 확인 여부
 
 //아이디 중복 확인
 function printId() {
@@ -24,22 +34,36 @@ function printRepw() {
 function check_pw() {
   if (pw === repw) {
     alert("확인했습니다");
+    isPwValid = true;
   } else {
     alert("비밀번호가 맞지 않습니다.");
+    isPwValid = false;
   }
+  checkForm();
 }
 function check() {
+  // console.log("id", typeof id);
+  // console.log("localid", typeof local_data_id);
   /*input에 있는 id를 가지고 와서 비교 후 alert을 띄움*/
-  let local_data = localStorage.getItem("data");
-  console.log("local", JSON.parse(local_data));
+  if (local_data_id.indexOf(id) >= 0) {
+    isIdValid = false;
+    alert("아이디가 이미 있습니다.");
+  } else {
+    isIdValid = true;
+    alert("사용가능합니다.");
+  }
 
-  JSON.parse(local_data).map((element) => {
-    if (Number(id) == Number(element.id)) {
-      alert("아이디가 이미 있습니다.");
-      return;
-    } else {
-      alert("사용가능합니다.");
-      return;
-    }
-  });
+  checkForm();
 }
+
+function checkForm() {
+  if (isIdValid && isPwValid) {
+    btn_join.disabled = false; // 아이디와 비밀번호가 모두 유효하면 버튼 활성화
+  } else {
+    btn_join.disabled = true; // 하나라도 유효하지 않으면 버튼 비활성화
+  }
+}
+
+window.onload = function () {
+  btn_join.disabled = true;
+};
